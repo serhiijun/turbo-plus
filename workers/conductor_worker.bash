e97aac64b24be5749bcd87be767ae26f31f7e0ad
Serhii Hiba <enotikalt@gmail.com>
2022-03-28 13:32:20 +0300

    test

--- workers/conductor_worker.bash
+++ workers/conductor_worker.bash
@@ -3,15 +3,16 @@
 #-> production folder:
 #-> /home/deployuser/apps/#{app name where you ENTERED command}
 testt () {
-  FILES="/Users/enotikalt/testone/*"
-  FILES2="/Users/enotikalt/testtwo/"
+  FILES="$HOME/testone/*"
+  FILES2="$HOME/testtwo/"
   for f in $FILES; do
     if [ -d "${f}" ]; then
       #->content (files and subfolders)
       #copy to folder
       #->which will be created (folder means)
       echo "${f} is a directory";
-      cp -RT "$f" "$FILES2$(basename \"$f\")"
+      # cp -RT "$f" "$FILES2$(basename \"$f\")"
+      cp -RT "$f" "$FILES2"
     elif [ -f "${f}" ]; then
       echo "${f} is a file";
     else
