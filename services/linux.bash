bb3f1acdeed6031002bfd1a161ccb04813de389a
Serhii Hiba <enotikalt@gmail.com>
2022-10-26 17:43:28 +0300

    Update linux.bash

--- services/linux.bash
+++ services/linux.bash
@@ -6,3 +6,17 @@
 #
 #-> systemctl is-enabled name.service
 # Check if service is enabled for autoload
+#
+#
+#
+#
+# PSQL DUMP
+# Take snapshot
+#-> pg_dump -U username --clear mydatabase_name > dev_17_18_wed_26.sql
+#
+# Copy from first server to second (current) server (or computer)
+#-> scp serhii@134.122.30.250:/home/username/dev_17_18_wed_26.sql /home/username/downloaded_dev_17_18_wed_26.sql
+#
+# Overwrite existing DB with downloaded snapshot file
+#-> psql -U username mydatabase_name < /home/serhii/downloaded_dev_17_18_wed_26.sql
+#
